eff6f7ec1b592780b0056d7f81d3d9dcb26d8d39
Marco Masetti <grubert65@gmail.com>
2017-07-21 18:54:06 +0200

    Fixed issues related with request payload handling (#3)

--- client_test.go
+++ client_test.go
@@ -2,6 +2,7 @@ package rest
 
 import (
 	"encoding/base64"
+	"encoding/json"
 	"encoding/xml"
 	"fmt"
 	"github.com/stretchr/testify/assert"
@@ -115,7 +116,7 @@ type XMLFoo struct {
 	Foo     string   `xml:"foo"`
 }
 
-func TestHttpJSONReq(t *testing.T) {
+func TestHttpJSONResp(t *testing.T) {
 
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
@@ -123,6 +124,8 @@ func TestHttpJSONReq(t *testing.T) {
 	}))
 	defer ts.Close()
 
+	headers := make(map[string]string)
+	headers["Content-Type"] = "application/json"
 	client := NewClient(ts.URL, "", "", false, true, nil)
 
 	api := NewBaseAPI(http.MethodGet, "/", nil, new(JSONFoo), nil)
@@ -135,7 +138,7 @@ func TestHttpJSONReq(t *testing.T) {
 	assert.Equal(t, "bar", resp.Fields["foo"])
 }
 
-func TestHttpXMLReq(t *testing.T) {
+func TestHttpXMLResp(t *testing.T) {
 
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/xml")
@@ -143,7 +146,9 @@ func TestHttpXMLReq(t *testing.T) {
 	}))
 	defer ts.Close()
 
-	client := NewClient(ts.URL, "", "", false, true, nil)
+	headers := make(map[string]string)
+	headers["Content-Type"] = "application/xml"
+	client := NewClient(ts.URL, "", "", false, true, headers)
 
 	api := NewBaseAPI(http.MethodGet, "/", nil, new(XMLFoo), nil)
 	err := client.Do(api)
@@ -155,11 +160,33 @@ func TestHttpXMLReq(t *testing.T) {
 	assert.Equal(t, "bar", resp.Foo)
 }
 
-func TestHttpOctetStreamReq(t *testing.T) {
+func TestHttpOctetStreamResp(t *testing.T) {
 
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/octet-stream")
 		w.Write([]byte(`ouitybw50ybvqy9yt8b6983p8v93`))
+	}))
+	defer ts.Close()
+
+	headers := make(map[string]string)
+	headers["Content-Type"] = "application/octet-stream"
+	client := NewClient(ts.URL, "", "", false, true, headers)
+
+	api := NewBaseAPI(http.MethodGet, "/", nil, new([]byte), nil)
+	err := client.Do(api)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+	}
+	assert.Equal(t, http.StatusOK, api.StatusCode())
+	buffer := *api.ResponseObject().(*[]byte)
+	assert.Equal(t, []byte("ouitybw50ybvqy9yt8b6983p8v93"), buffer)
+}
+
+func TestHttpNoBodyResp(t *testing.T) {
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		//w.Header().Set("Content-Type", "application/octet-stream")
+		//w.Write([]byte(`ouitybw50ybvqy9yt8b6983p8v93`))
 	}))
 	defer ts.Close()
 
@@ -172,5 +199,81 @@ func TestHttpOctetStreamReq(t *testing.T) {
 	}
 	assert.Equal(t, http.StatusOK, api.StatusCode())
 	buffer := *api.ResponseObject().(*[]byte)
-	assert.Equal(t, []byte("ouitybw50ybvqy9yt8b6983p8v93"), buffer)
+	assert.Equal(t, []uint8([]byte(nil)), buffer)
+}
+
+type ErrStruct struct {
+	ErrID   string `json:"error_id"`
+	ErrCode string `json:"error_code"`
+	ErrText string `json:"error_text"`
+}
+
+func TestHttpErrorObjectBack(t *testing.T) {
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"error_id":"001","error_code":"12345","error_text":"Foo bar"}`))
+	}))
+	defer ts.Close()
+
+	client := NewClient(ts.URL, "", "", false, true, nil)
+
+	api := NewBaseAPI(http.MethodGet, "/", nil, new([]byte), new(ErrStruct))
+	err := client.Do(api)
+	if err != nil {
+		assert.Equal(t, http.StatusInternalServerError, api.StatusCode())
+		errStruct := api.ErrorObject().(*ErrStruct)
+		assert.Equal(t, "001", errStruct.ErrID)
+		assert.Equal(t, "12345", errStruct.ErrCode)
+	}
+}
+
+type ReqBody struct {
+	FieldOne string `json:"field_1"`
+	FieldTwo string `json:"field_2"`
+}
+
+func TestHttpRequestPayload(t *testing.T) {
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		assert.Equal(t, "PUT", r.Method)
+		r.ParseForm()
+
+		var reqPayload ReqBody
+		decBody := json.NewDecoder(r.Body)
+		decBody.Decode(&reqPayload)
+		//assert.Equal(t, nil, err)
+		assert.Equal(t, "Foo", reqPayload.FieldOne)
+		assert.Equal(t, "Bar", reqPayload.FieldTwo)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"field_1":"Fooo", "field_2":"Baar"}`))
+	}))
+	defer ts.Close()
+
+	headers := make(map[string]string)
+	headers["Content-Type"] = "application/json"
+	client := NewClient(ts.URL, "", "", false, true, headers)
+
+	var reqPayload ReqBody
+	reqPayload.FieldOne = "Foo"
+	reqPayload.FieldTwo = "Bar"
+	assert.Equal(t, "Foo", reqPayload.FieldOne)
+	assert.Equal(t, "Bar", reqPayload.FieldTwo)
+
+	api := NewBaseAPI(http.MethodPut, "/", reqPayload, new(ReqBody), new(ErrStruct))
+	err := client.Do(api)
+	if err != nil {
+		assert.Equal(t, http.StatusInternalServerError, api.StatusCode())
+		errStruct := api.ErrorObject().(*ErrStruct)
+		assert.Equal(t, "001", errStruct.ErrID)
+		assert.Equal(t, "12345", errStruct.ErrCode)
+	}
+
+	respBody := *api.ResponseObject().(*ReqBody)
+	assert.Equal(t, "Fooo", respBody.FieldOne)
+	assert.Equal(t, "Baar", respBody.FieldTwo)
 }
